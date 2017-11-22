f4217ec71b6bc7512a35c2c994e0553f78e6b0f1
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2017-11-22 12:01:34 +0700

    Use config file for all configuration setup

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,26 @@ extern crate rusqlite;
 #[macro_use] extern crate lazy_static;
 
 use std::io;
-use std::fmt;
-use std::env;
 use std::sync::Mutex;
 
-mod id;
-mod url;
-
-use id::Id;
-use rocket::{State, Request};
+use rocket::{Rocket, Request, State};
 use rocket::request::Form;
 use rocket::response::{Redirect, NamedFile};
 use rocket::response::status::Created;
 use rocket::fairing::AdHoc;
 use rusqlite::{Connection};
 
-const ID_LENGTH: usize = 3;
+mod id;
+mod url;
+
+use id::Id;
+
+
 type Db = Mutex<Connection>;
 
-struct Address(String);
-
-
-impl fmt::Display for Address {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.0)
-    }
+struct Config {
+    address: String,
+    id_length: usize,
 }
 
 
@@ -78,11 +73,11 @@ fn retrieve(id: Id, db: State<Db>) -> Option<Redirect> {
 
 
 #[post("/", data = "<url>")]
-fn create(url: Form<url::Data>, db: State<Db>, address: State<Address>)
+fn create(url: Form<url::Data>, db: State<Db>, config: State<Config>)
           -> Result<Created<String>, &'static str> {
     use rusqlite::Error::*;
 
-    let id = id::Id::new(ID_LENGTH);
+    let id = id::Id::new(config.id_length);
     let conn = db.lock().expect("db connection lock");
     let url = url.get();
     match get_url(&id, &conn) {
@@ -90,7 +85,8 @@ fn create(url: Form<url::Data>, db: State<Db>, address: State<Address>)
         Err(QueryReturnedNoRows) => {
             insert_url(&id, &url, &conn)
                 .map(|()| {
-                    Created(format!("{address}/{id}", address=&*address, id=id),
+                    Created(format!("{address}/{id}",
+                                    address=config.address, id=id),
                             Some(format!("{url}", url=url)))
                 })
                 .map_err(|_| "cannot create id")
@@ -100,8 +96,8 @@ fn create(url: Form<url::Data>, db: State<Db>, address: State<Address>)
 }
 
 
-#[post("/<id>", data="<url>")]
-fn create_with_id(id: Id, url: Form<url::Data>) -> String {
+#[post("/<_id>", data="<_url>")]
+fn create_with_id(_id: Id, _url: Form<url::Data>) -> String {
     format!("another unimplemented")
 }
 
@@ -112,27 +108,38 @@ fn not_found(req: &Request) -> String {
 }
 
 
-fn main() {
-    let mut args = env::args_os();
-    args.next(); // ignore command name
-    let conn = if let Some(db_name) = args.next() {
-        Connection::open(db_name).expect("sqlite database file")
-    } else {
-        eprintln!("Using in memory database");
-        let conn = Connection::open_in_memory().expect("in memory db");
-        init_db(&conn);
-        conn
+fn init_config(rocket: Rocket) -> Result<Rocket, Rocket> {
+    // get db_file config, use in memory database if it's not available
+    let conn = match rocket.config().get_str("db_file") {
+        Ok(db_file) => {
+            Connection::open(db_file)
+                .expect("sqlite3 database file")
+        },
+        _ => {
+            let conn = Connection::open_in_memory()
+                .expect("in memory db");
+            init_db(&conn);
+            conn
+        }
     };
 
+    // get other config setup
+    let address = rocket.config()
+        .get_str("display_address")
+        .unwrap_or("http://localhost:8000")
+        .to_string();
+    let id_length = rocket.config()
+        .get_int("id_length")
+        .unwrap_or(5) as usize;
+
+    Ok(rocket
+       .manage(Mutex::new(conn))
+       .manage(Config { address, id_length }))
+}
+
+fn main() {
     rocket::ignite()
-        .attach(AdHoc::on_attach(|rocket| {
-            let address = rocket.config()
-                .get_str("display_address")
-                .unwrap_or("http://localhost:8000")
-                .to_string();
-            Ok(rocket.manage(Address(address)))
-        }))
-        .manage(Mutex::new(conn))
+        .attach(AdHoc::on_attach(init_config))
         .mount("/", routes![index, retrieve, create, create_with_id])
         .catch(errors![not_found])
         .launch();
